b8616df147e9f05f0619ce363b1efaa56829a785
Matthew Percy <matt@4pi.com.au>
2018-05-17 08:31:17 +1000

    Added the ability to configure text style, font size and gravity Changed the way the dismiss button works. It now works as a Skip button Fixed a bug in RemoveFromWindow where we didnt dettech from the ViewTreeObserver properly because we already were removed from the parent view

--- MaterialShowcaseViewCore/ShowcaseConfig.cs
+++ MaterialShowcaseViewCore/ShowcaseConfig.cs
@@ -1,22 +1,35 @@
 ﻿using Android.Graphics;
+using Android.Views;
 using MaterialShowcaseViewCore.Shape;
 
-namespace MaterialShowcaseViewCore {
-    public class ShowcaseConfig {
-        public static readonly Color DefaultMaskColour = Color.ParseColor("#dd335075");
-        public const long DefaultFadeTime = 300;
-        public const long DefaultDelay = 0;
-        public static readonly IShape DefaultShape = new CircleShape();
-        public const int DefaultShapePadding = 10;
+namespace MaterialShowcaseViewCore
+{
+	public class ShowcaseConfig
+	{
+		public static readonly Color DefaultMaskColour = Color.ParseColor("#dd335075");
+		public const long DefaultFadeTime = 300;
+		public const long DefaultDelay = 0;
+		public static readonly IShape DefaultShape = new CircleShape();
+		public const int DefaultShapePadding = 10;
 
-        public long Delay { get; set; } = DefaultDelay;
-        public Color MaskColor { get; set; } = DefaultMaskColour;
-        public Color ContentTextColor { get; set; } = Color.ParseColor("#ffffff");
-        public Color DismissTextColor { get; set; } = Color.ParseColor("#ffffff");
-        public Typeface DismissTextStyle { get; set; } = Typeface.DefaultBold;
-        public long FadeDuration { get; set; } = DefaultFadeTime;
-        public IShape Shape { get; set; } = DefaultShape;
-        public int ShapePadding { get; set; } = DefaultShapePadding;
-        public bool RenderOverNavigationBar { get; set; }
-    }
+		public long Delay { get; set; } = DefaultDelay;
+		public Color MaskColor { get; set; } = DefaultMaskColour;
+		public Color ContentTextColor { get; set; } = Color.ParseColor("#ffffff");
+		public Color TitleTextColor { get; set; } = Color.ParseColor("#ffffff");
+		public Color DismissTextColor { get; set; } = Color.ParseColor("#ffffff");
+		public Typeface TitleTextStyle { get; set; } = Typeface.DefaultBold;
+		public Typeface DismissTextStyle { get; set; } = Typeface.DefaultBold;
+		public Typeface ContentTextStyle { get; set; } = Typeface.Default;
+		public GravityFlags TitleTextGravity { get; set; } = GravityFlags.Center;
+		public GravityFlags DismissTextGravity { get; set; } = GravityFlags.Center;
+		public GravityFlags ContentTextGravity { get; set; } = GravityFlags.Center;
+		public float TitleTextFontSize { get; set; } = 30;
+		public float DismissTextFontSize { get; set; } = 15;
+		public float ContentTextFontSize { get; set; } = 15;
+
+		public long FadeDuration { get; set; } = DefaultFadeTime;
+		public IShape Shape { get; set; } = DefaultShape;
+		public int ShapePadding { get; set; } = DefaultShapePadding;
+		public bool RenderOverNavigationBar { get; set; }
+	}
 }
